e1995d1e3d45b1d8babea4ba83e004552a3460c4
Peng YANG <yangpeng@harmonycloud.cn>
2022-10-08 20:04:32 +0800

    part 1

---
+++ src/views/LifeCycles.vue
@@ -0,0 +1,58 @@
+<template>
+  <p>lifecycles demo</p>
+</template>
+
+<!-- <script lang="ts">
+export default {
+  beforeCreate() {
+    console.log("beforeCreate");
+  },
+  created() {
+    console.log("created");
+  },
+  beforeMount() {
+    console.log("beforeMount");
+  },
+  mounted() {
+    console.log("mounted");
+  },
+  beforeUpdate() {
+    console.log("beforeUpdate");
+  },
+  updated() {
+    console.log("updated");
+  },
+  beforeUnmount() {
+    console.log("beforeUnmount");
+  },
+  unmounted() {
+    console.log("unmounted");
+  },
+};
+</script> -->
+
+<script lang="ts">
+import { onBeforeMount, onBeforeUnmount, onBeforeUpdate, onMounted, onUnmounted, onUpdated } from 'vue'
+export default {
+  setup() {
+    onBeforeMount(() => {
+      console.log("onBeforeMount");
+    })
+    onMounted(() => {
+      console.log("onMounted");
+    })
+    onBeforeUpdate(() => {
+      console.log("onBeforeUpdate");
+    })
+    onUpdated(() => {
+      console.log("onUpdated");
+    })
+    onBeforeUnmount(() => {
+      console.log("onBeforeUnmount");
+    })
+    onUnmounted(() => {
+      console.log("onUnmounted");
+    })
+  }
+}
+</script>
